bdd9f99f1aead8d82f36883b31717a66b8fb154b
shreyasheda <shreyasheda1992@gmail.com>
2022-03-09 11:20:25 +0530

    prod setting

--- server.js
+++ server.js
@@ -1,17 +1,13 @@
-function requireHTTPS(req, res, next) {
-  // The 'x-forwarded-proto' check is for Heroku
-  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-      return res.redirect('https://' + req.get('host') + req.url);
-  }
-  next();
-}
 const express = require('express');
+const path = require('path');
+
 const app = express();
-app.use(requireHTTPS);
+// Serve only the static files form the dist directory
 app.use(express.static('./dist/my-first-app'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: './dist/my-first-app/'}),
+    res.sendFile('index.html', {root: 'dist/my-first-app/'}),
 );
 
-app.listen(process.env.PORT || 5000);
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
